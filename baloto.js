d064ec66e3fc2157670ea6772391c4c648c0a947
Ivan Hernandez <134563888+ihernandez-cripto@users.noreply.github.com>
2024-11-25 16:11:26 -0500

    cambio de variable resultado

--- baloto.js
+++ baloto.js
@@ -3,7 +3,8 @@ const colorLoto = ['B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'M1', 'M2', 'M3', '
 const baLoto = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42','43'];
 const miLoto = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39'];
 const superBalota = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16'];
-
+// Arreglo para almacenar los resultados
+let resultados = [];
 // Función para seleccionar y eliminar un elemento aleatorio
 function seleccionarAleatorio(array) {
   const indiceAleatorio = Math.floor(Math.random() * array.length);
@@ -13,8 +14,7 @@ function seleccionarAleatorio(array) {
 }
 
 function mezclar(nombreArreglo, numeroApostar){
-    // Arreglo para almacenar los resultados
-    const resultados = [];
+    resultados = [];
     const arregloUtilizado = eval(nombreArreglo);
     for (let i = 0; i < numeroApostar; i++) {
         const elementoSeleccionado = seleccionarAleatorio(arregloUtilizado);
