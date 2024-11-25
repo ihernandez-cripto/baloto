0a1ba3dc65ef0a6dbf1f2f9bdeae2d65470b7423
Ivan Hernandez <134563888+ihernandez-cripto@users.noreply.github.com>
2024-11-25 16:05:54 -0500

    inicializar resultado

--- baloto.js
+++ baloto.js
@@ -4,9 +4,6 @@ const baLoto = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '
 const miLoto = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39'];
 const superBalota = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16'];
 
-// Arreglo para almacenar los resultados
-const resultados = [];
-
 // Función para seleccionar y eliminar un elemento aleatorio
 function seleccionarAleatorio(array) {
   const indiceAleatorio = Math.floor(Math.random() * array.length);
@@ -16,6 +13,8 @@ function seleccionarAleatorio(array) {
 }
 
 function mezclar(nombreArreglo, numeroApostar){
+    // Arreglo para almacenar los resultados
+    const resultados = [];
     const arregloUtilizado = eval(nombreArreglo);
     for (let i = 0; i < numeroApostar; i++) {
         const elementoSeleccionado = seleccionarAleatorio(arregloUtilizado);
